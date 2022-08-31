a09cf4152d5cded2ed7e66f026f395db6fbba462
Jamie Hurd <jh735920@broadcom.net>
2022-08-31 14:08:42 -0600

    Multi-page website uses reactrouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
+import
+{
+    BrowserRouter as Router, Routes, Route
+}
 import './SCSS/index.css';
 
-function App() {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+export default function App()
+{
+    return
+    (
+        <div>
+            <h1>Portfolio!</h1>
+        </div>
+    );
 }
 
 export default App;
