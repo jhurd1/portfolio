daf72372577bafda0336dce6e9d965b891e7c718
Jamie Hurd <jh735920@broadcom.net>
2022-08-31 14:37:48 -0600

    App.js jsx in place for nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import
-{
-    BrowserRouter as Router, Routes, Route
-}
+import { Link } from "react-router-dom";
 import './SCSS/index.css';
 
 export default function App()
@@ -10,7 +7,14 @@ export default function App()
     return
     (
         <div>
-            <h1>Portfolio!</h1>
+            <h1>Jamie Hurd</h1>
+            <h2 class="h2">A portfolio.</h2>
+         <nav>
+            <Link to="/AboutMe"></Link> | {" "}
+            <Link to="/GitHub Projects">GitHub Projects</Link>
+            <Link to="/Photography">Photopraphy</Link>
+            <Link to="/Influence">Influence</Link>
+        </nav>
         </div>
     );
 }
