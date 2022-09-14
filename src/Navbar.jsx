909c00949c9834f0383ba210a525bc406dc84b9c
Jamie Hurd <jh735920@broadcom.net>
2022-09-14 13:15:36 -0600

    Successful test of css import for Navbar.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./SCSS/index.css";
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/Home"></Link>
-                </li>
-                    <Link to="/AboutMe"></Link> | {" "}
-                <li>
-                    <Link to="/GitHub Projects">GitHub Projects</Link>
-                </li>
-                    <Link to="/Photography">Photopraphy</Link>
-                <li>
-                    <Link to="/Influence">Influence</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+        <div id="menu-outer">
+            <div class="table">
+                <ul id="horizontal-list">
+                    <nav id="menu-outer">
+                        
+                            <li>
+                                <Link to="/Home"></Link>
+                            </li>
+                                <Link to="/AboutMe"></Link> | {" "}
+                            <li>
+                                <Link to="/GitHub Projects">GitHub Projects</Link>
+                            </li>
+                                <Link to="/Photography">Photopraphy</Link>
+                            <li>
+                                <Link to="/Influence">Influence</Link>
+                            </li>
+                        
+                    </nav>
+                </ul>
+            </div>
+        </div>
+
+            );
 }
 
 export default Navbar;
