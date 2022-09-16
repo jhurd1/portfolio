76831ea14ebf04ee08454dfafe9e4fce5a2a77d5
jhurd1 <jamiehurd3@gmail.com>
2022-09-15 20:41:56 -0600

    Testing additional HTML tags with JSX return statement; requires fragment operators.

--- src/Github.jsx
+++ src/Github.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 function Github() {
     return
     (
+        <>
         <h1>My Github</h1>
+        <p>Click here for a view of some of my software projects on Github.</p>;
+        </>
     );
 }
 
